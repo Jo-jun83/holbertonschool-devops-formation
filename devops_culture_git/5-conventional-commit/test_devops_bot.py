ef61405b40d3cac6356e7cd3f2be8faf50f68960
Jonas-Jungling <jonas.jungling@oclock.school>
2026-09-11 11:30:33 +0200

    test: add energy validation tests

---
+++ devops_culture_git/5-conventional-commit/test_devops_bot.py
@@ -0,0 +1,19 @@
+import unittest
+
+from devops_bot import validate_energy
+
+
+class TestValidateEnergy(unittest.TestCase):
+
+    def test_normal_value(self):
+        self.assertEqual(validate_energy(50), 50)
+
+    def test_value_below_zero(self):
+        self.assertEqual(validate_energy(-20), 0)
+
+    def test_value_above_one_hundred(self):
+        self.assertEqual(validate_energy(150), 100)
+
+
+if __name__ == "__main__":
+    unittest.main()
